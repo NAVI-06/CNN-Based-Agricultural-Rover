1b7fa033e6b5697a38b7ebbfb39d008251c5db7e
NAVI-06 <146514786+NAVI-06@users.noreply.github.com>
2025-05-15 16:10:41 +0530

    train.py

---
+++ train.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_dir = 'dataset/train'
+valid_dir = 'dataset/test'
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,  # Normalize pixel values to [0, 1]
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+valid_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+valid_generator = valid_datagen.flow_from_directory(
+    valid_dir,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+# Build the CNN model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+
+    Dense(len(train_generator.class_indices), activation='softmax')  # Output layer
+])
+
+# Compile the model
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train the model
+history = model.fit(
+    train_generator,
+    epochs=20,
+    validation_data=valid_generator
+)
+
+# Save the model for later use.
+model.save('my_keras_cotton')
